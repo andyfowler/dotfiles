5e8210f0beda29d99953d9df63c23801f6ed0368
Andy Fowler <afowler@nutshell.com>
2012-08-20 10:45:08 -0400

    bunches of updates, dont feel like enumerating

--- afowler.zsh
+++ afowler.zsh
@@ -6,9 +6,13 @@ alias pgrep='ps auxwww | grep'
 alias less="less -r"
 
 # thanks ablyler :)
-alias metar='curl -s -d "station_ids=karb,kozw,kyip,kptk,kphn,kadg,kdtw,kttf,konz,klan,kjxn,ktol,kduh,kgrr" http://aviationweather.gov/adds/metars/index.php | html2text -width 100 | sed "1,4d"'
+alias metar='curl -s "http://aviationweather.gov/adds/metars/index.php?station_ids=karb,kozw,kyip,kptk,kphn,kadg,kdtw,kttf,konz,klan,kjxn,ktol,kduh,ktvc,kmdw,kgrr,kcxy,kmdt,kcle&std_trans=standard&chk_metars=on&hoursStr=most+recent+only&submitmet=Submit"  | html2text -width 255 | sed "1,4d"'
 
-alias taf='curl -s "http://aviationweather.gov/adds/metars/index.php?submit=1&station_ids=karb,kdtw,kjxn,klan,kozw,kyip,kttf,konz,kdet,ktol,kgrr&chk_tafs=on" | grep -v "<"'
+alias taf='curl -s "http://aviationweather.gov/adds/metars/index.php?submit=1&station_ids=karb,kdtw,kjxn,klan,kozw,kyip,kttf,konz,kdet,ktol,kgrr,kcxy,kmdt,kcle&chk_tafs=on" | grep -v "<"'
+
+alias notams='curl -s "https://pilotweb.nas.faa.gov/PilotWeb/notamRetrievalByICAOAction.do?method=displayByICAOs&formatType=DOMESTIC&retrieveLocId=karb&reportType=RAW&actionType=notamRetrievalByICAOs" |pcregrep -M -o1 -N CRLF -i "<pre>([^<]*)"'
+
+export NODE_PATH=/usr/local/lib/node_modules
 
 bindkey "^U" backward-kill-line
 unsetopt auto_pushd
@@ -21,7 +25,7 @@ else # local shell
   ZSH_THEME_TERM_TITLE_IDLE="%~"
 fi
 
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
+# [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
 
 # Appears at the beginning of (and during) of command execution
 function preexec {
