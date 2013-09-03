5d97870b5282588edec1cd7cb7424cf34c0e29ab
Andy Fowler <afowler@nutshell.com>
2013-09-03 09:43:59 -0400

    load aws completions if available
    from `pip install awscli`

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,7 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+if [[ -a "/usr/local/bin/aws_zsh_completer.sh" ]]; then
+	source /usr/local/bin/aws_zsh_completer.sh
+fi
