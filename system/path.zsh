6de0f820d623eb029a9c1a9e831b8522fc630d18
Andy Fowler <afowler@nutshell.com>
2015-11-12 18:14:28 -0500

    update paths for golang stuff

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,5 @@
-export PATH="$HOME/.composer/vendor/bin/:/usr/local/share/npm/bin:/usr/local/opt/ruby/bin:$HOME/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export PATH="$HOME/.composer/vendor/bin/:/usr/local/share/npm/bin:/usr/local/opt/ruby/bin:$HOME/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/source/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+export GOPATH="$HOME/source"
