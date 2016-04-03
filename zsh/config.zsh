f2c61801c55345c365bf5313f0b902a2dc7a1a6c
Andy Fowler <afowler@nutshell.com>
2016-04-03 18:25:01 -0400

    fixup / tweak zsh history options

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -6,31 +6,32 @@ export CLICOLOR=true
 #autoload -U $ZSH/zsh/functions/*(:t)
 
 HISTFILE=~/.zsh_history
-HISTSIZE=10000
-SAVEHIST=10000
+HISTSIZE=1000000
+SAVEHIST=1000000
 
 setopt NO_BG_NICE # don't nice background tasks
 setopt NO_HUP
 setopt NO_LIST_BEEP
 setopt LOCAL_OPTIONS # allow functions to have local options
 setopt LOCAL_TRAPS # allow functions to have local traps
-setopt HIST_VERIFY
-setopt SHARE_HISTORY # share history between sessions ???
+
+# history stuff
+setopt HIST_VERIFY # don't immediately run a command when loading it with something like !$, but show for preview
+setopt SHARE_HISTORY # share history between sessions
 setopt EXTENDED_HISTORY # add timestamps to history
-setopt PROMPT_SUBST
+setopt APPEND_HISTORY # adds history, instead of obliterating the entire file per shell
+setopt HIST_IGNORE_DUPS  # don't record dupes in history, if they happen directly after each other (but en masse dupes allowed)
+setopt HIST_REDUCE_BLANKS # i think this means trim commands, docs unclear...
+#setopt INC_APPEND_HISTORY # this is implied by SHARE_HISTORY, and docs say not to do both
+
 setopt CORRECT
 setopt COMPLETE_IN_WORD
 setopt IGNORE_EOF
 setopt EXTENDED_GLOB
 
-setopt APPEND_HISTORY # adds history
-setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
-setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
-setopt HIST_REDUCE_BLANKS
-
 # don't expand aliases _before_ completion has finished
 #   like: git comm-[tab]
-setopt complete_aliases
+setopt COMPLETE_ALIASES
 
 #zle -N newtab
 
