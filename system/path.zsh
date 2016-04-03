1e525fe3488d9376b41ba8e5bc469a6403f75a03
Andy Fowler <afowler@nutshell.com>
2016-04-03 16:03:02 -0400

    add local php56 to path if exists

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,8 @@
-export PATH="$HOME/.composer/vendor/bin/:/usr/local/share/npm/bin:/usr/local/opt/ruby/bin:$HOME/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/source/bin:$PATH"
+export PATH="$HOME/.composer/vendor/bin:/usr/local/share/npm/bin:/usr/local/opt/ruby/bin:$HOME/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/source/bin:$PATH"
+if which brew &> /dev/null
+then
+  export PATH="$(brew --prefix php56)/bin:$PATH"
+fi
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
