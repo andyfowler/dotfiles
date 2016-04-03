82a11703227d6a4b701da617b16350b83b96077d
Andy Fowler <afowler@nutshell.com>
2016-04-03 19:51:03 -0400

    don’t shell out to brew for a simple string

--- system/path.zsh
+++ system/path.zsh
@@ -1,7 +1,7 @@
 export PATH="$HOME/.composer/vendor/bin:/usr/local/share/npm/bin:/usr/local/opt/ruby/bin:$HOME/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/source/bin:$PATH"
-if which brew &> /dev/null
+if [[ -d /usr/local/opt/php56/bin ]]; # from homebrew, but brew --prefix is slow
 then
-  export PATH="$(brew --prefix php56)/bin:$PATH"
+  export PATH="/usr/local/opt/php56/bin:$PATH"
 fi
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
