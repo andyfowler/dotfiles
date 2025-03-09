db222326953cdc518bdbe1238c6fc846060e801c
Andy Fowler <afowler@nutshell.com>
2025-03-09 15:27:27 +0000

    remove old php path, add duckdb

--- system/path.zsh
+++ system/path.zsh
@@ -1,7 +1,7 @@
 export PATH="$HOME/.composer/vendor/bin:/usr/local/share/npm/bin:/usr/local/opt/ruby/bin:$HOME/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/source/bin:$PATH"
-if [[ -d /usr/local/opt/php56/bin ]]; # from homebrew, but brew --prefix is slow
+if [[ -d $HOME/.duckdb/cli/latest ]]; 
 then
-  export PATH="/usr/local/opt/php56/bin:$PATH"
+  export PATH="$HOME/.duckdb/cli/latest:$PATH"
 fi
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
